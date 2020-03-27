77bfda3a44461b4e37bc58b407fce2760a29b5cf
HoMinh Chung <hmchung92@gmail.com>
2020-03-28 04:34:13 +0700

    feat(lab07): cleanup

--- Lab07/app/src/main/java/vn/edu/tdtu/lab07/exercise04/AppExecutors.java
+++ Lab07/app/src/main/java/vn/edu/tdtu/lab07/exercise04/AppExecutors.java
@@ -9,9 +9,6 @@ import java.util.concurrent.Executors;
 
 /**
  * Global executor pools for the whole application.
- * <p>
- * Grouping tasks like this avoids the effects of task starvation (e.g. disk reads don't wait behind
- * webservice requests).
  */
 public class AppExecutors {
   // For Singleton instantiation
--- Lab07/app/src/main/java/vn/edu/tdtu/lab07/exercise04/EventAdaptor.java
+++ Lab07/app/src/main/java/vn/edu/tdtu/lab07/exercise04/EventAdaptor.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnCreateContextMenuListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.Switch;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
